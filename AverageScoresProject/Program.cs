3e7c8117179d5a4fe72e2520d02fc38cd225951a
brail33na11@gmail.com <brail33na11@gmail.com>
2019-07-25 09:33:24 -0400

    Add project files.

---
+++ AverageScoresProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AverageScoresProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
